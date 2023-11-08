f5d4697d45375c44ba975b41a10b420c660b45bb
Valentyn Lysenko <valentynlysenco@gmail.com>
2023-11-08 17:21:22 +0200

    feat: add js file

---
+++ index.js
@@ -0,0 +1,49 @@
+// ................input.................//
+
+
+//   document.querySelector('.button_1').addEventListener('click', () => {
+//   let inp = document.querySelector('.input_1').value;
+//   document.querySelector('.content_1').innerHTML += inp + '    ';
+
+//   // document.querySelector('.input_1').value = 'Valentin';
+// })
+
+// // Range "Діапазон"
+
+// document.querySelector('.input_2').addEventListener('input', () => {
+//   let inp = document.querySelector('.input_2').value;
+//   document.querySelector('.content_2').innerHTML = inp;
+// })
+
+// // CheckBox _checked
+
+// document.querySelector('.button_3').addEventListener('click', () => {
+//   // let inp = document.querySelector('.input_3').value;
+//   // if (document.querySelector('.input_3').checked) {
+//   //   document.querySelector('.content_3').innerHTML = inp;
+//   // } else {
+//   //   document.querySelector('.content_3').innerHTML = '';
+//   // }
+
+//   if (document.querySelector('.input_3').checked) {
+//     document.querySelector('.input_3').checked = false;
+//   }
+//   else {
+//     document.querySelector('.input_3').checked = true;
+//   }
+// })
+
+// // radio Button
+
+// document.querySelector('.button_4').addEventListener('click', () => {
+//   let inp = document.querySelectorAll('.input_4');
+//   for (let i = 0; i < inp.length; i++) {
+//     if (inp[i].checked) {
+//       result = inp[i].value;
+//       break;
+//     }
+//   }
+//   document.querySelector('.content_4').innerHTML = result;
+// })
+
+
